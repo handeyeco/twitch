07c81747ec866e7de186a6eed7d768575f519ed6
Matthew Curtis <matthew@matthewbryancurtis.com>
2016-06-16 10:25:08 -0700

    Working (ugly) prototype complete

--- src/index.jsx
+++ src/index.jsx
@@ -7,8 +7,19 @@ var TwitchApp = React.createClass({
   getInitialState: function() {
     return {
       userObjs: [{displayName: "Loading"}],
-      selectedUser: ""
+      selectedUser: null
     };
+  },
+
+  handleSelection: function (event) {
+    var newUserName = event.target.innerHTML;
+    var newUserObj = userData.userObjs.filter(function (e){
+      return e.name === newUserName;
+    })[0];
+
+    this.setState(function(state, currentProps) {
+      return {selectedUser: newUserObj};
+    });
   },
 
   polling: function () {
@@ -26,26 +37,71 @@ var TwitchApp = React.createClass({
 
   render: function() {
     return (
-      <TwitchApp.userList userObjs={this.state.userObjs} />
+      <div>
+        <TwitchApp.header />
+        <TwitchApp.userList userObjs={this.state.userObjs} handleSelection={this.handleSelection} />
+        <TwitchApp.displayUser selectedUser={this.state.selectedUser} />
+      </div>
     )
   }
 });
 
-TwitchApp.userList = React.createClass({
-  handleSelection: function (event) {
-    console.log(event.target);
-  },
+TwitchApp.header = React.createClass({
+  render: function () {
+    return (
+      <nav>
+        <a href="https://www.twitch.tv/">
+          <img src="./images/twitch_logo.svg" />
+        </a>
+      </nav>
+    );
+  }
+});
 
+TwitchApp.userList = React.createClass({
   render: function() {
-    var userList = this.props.userObjs.map(function (element) {
-      return <li onClick={this.handleSelection} className="user-list-item" name={element.name} key={element.name}>{element.displayName.toUpperCase()}</li>
+    var userListItems = this.props.userObjs.map(function (element) {
+      return <li onClick={this.props.handleSelection} className={element.exist ? element.stream ? "streaming" : "quiet" : "error"} id={element.name} key={element.name}>{element.name}</li>
     }.bind(this));
 
     return (
       <div id="user-list-container">
-        <ul id="user-list">{userList}</ul>
+        <ul id="user-list">{userListItems}</ul>
       </div>
     )
+  }
+});
+
+TwitchApp.displayUser = React.createClass({
+  render: function() {
+    var userObj = this.props.selectedUser;
+
+    if (!userObj) {
+      return ( <h1 className="important-message">Welcome, please select a user</h1> );
+    }
+
+    if (userObj.exist) {
+      return (
+        <div id="user-display-container">
+          <img src={userObj.logo} />
+          <div className="user-data">
+            <a href={userObj.url}>
+              <h1>{userObj.displayName}</h1>
+            </a>
+            <p>Followers: {userObj.followers}</p>
+            <p>Views: {userObj.views}</p>
+            <p>Updated: {userObj.updated}</p>
+          </div>
+          <div className="user-status">
+            <p>Currently Streaming: {userObj.stream ? "Yes Game: " + userObj.game : "Nope"}</p>
+            <p>Status: {userObj.status}</p>
+          </div>
+        </div>
+      );
+    }
+
+    return ( <h1 className="important-message">Unable to find user</h1> );
+
   }
 });
 
